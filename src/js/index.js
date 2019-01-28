9c0cfa565518822a6591bdefba53a62e17627d33
Mary Em <lmary155@gmail.com>
2019-01-28 13:22:51 +0100

    babel config

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 // Global app controller
 import num from './test';
+const x = 23;
 
-
-console.log(`I imported ${num} from another module`);
+console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
+ 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['@babel/polyfill', './src/js/index.js'],
     output: {
-        path:path.resolve(__dirname, 'dist'),
-        filename:'js/bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
     },
     devServer: {
         contentBase: './dist'
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template:'./src/index.html'
+            template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
