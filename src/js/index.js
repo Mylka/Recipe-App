81fa110f289ff39fffbc491c754e4ffdaac66a1a
Mary Em <lmary155@gmail.com>
2019-01-29 21:29:08 +0100

    built search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+
 
 
 /** Global state of the app
@@ -12,25 +15,28 @@ import Search from './models/Search';
 
  const controlSearch = async () => {
     //1. get the query from the view
-    const query = 'pizza'//TODO
+    const query = searchView.getInput();
+    // console.log(query);
 
     if(query){
     //2. New search object and add to state
         state.search = new Search(query);
 
     //3. Prepare UI for the result
+        searchView.clearInput();
+        searchView.clearResults();
 
     //4. Search for recipes
          await state.search.getResults();
 
      //5. Render results on UI
-         console.log(state.search.result)
+         searchView.renderResults(state.search.result);
 
     }
 
  }
 
- document.querySelector('.search').addEventListener('submit', e =>{
+    elements.searchButton.addEventListener('submit', e =>{
      e.preventDefault();
      controlSearch();
  })
