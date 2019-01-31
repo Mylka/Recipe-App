e2ae2b244dd5f95ca16bfdc53ed2420e492770a1
Mary Em <lmary155@gmail.com>
2019-01-31 15:09:41 +0100

    Recipe Model/ parse ingredients

--- src/js/index.js
+++ src/js/index.js
@@ -34,16 +34,18 @@ import {elements, renderLoader, clearLoader} from './views/base';
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-
-    //4. Search for recipes
-         await state.search.getResults();
-
-     //5. Render results on UI
-         clearLoader();
-         searchView.renderResults(state.search.result);
-
+        try{
+            //4. Search for recipes
+                 await state.search.getResults();
+        
+             //5. Render results on UI
+                 clearLoader();
+                 searchView.renderResults(state.search.result);
+        }catch{
+            alert("Something went wrong...");
+            clearLoader();
+        }
     }
-
  }
 
     elements.searchButton.addEventListener('submit', e =>{
@@ -68,11 +70,41 @@ import {elements, renderLoader, clearLoader} from './views/base';
  * Recipe Controller
  */
 
- const r = new Recipe(46956)
- r.getRecipe();
- console.log(r);
 
+const controlRecipe = async () => {
+    //Get id frm the url
+    const id = window.location.hash.replace('#','');
+    console.log(id);
 
+    if(id){
+        
+    //prepare UI for changes
+
+    //create new recipe object
+        state.recipe = new Recipe(id)
+
+        try{
+            
+            //get recipe data and parse ingredients
+                 await state.recipe.getRecipe();
+                 state.recipe.parseIngredients();
+        
+            //Call calctime and calcserving
+                state.recipe.calcTime();
+                state.recipe.calcServings;
+        
+            // render the recipe
+                console.log(state.recipe);
+        }
+        catch(error){
+            alert("Error processing recipe!")
+        }
+    }
+
+};
+
+window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe);
 
 
 
